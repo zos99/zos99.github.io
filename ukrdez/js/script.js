b4fe54093140db9dea1944561709fc245a66bb50
Oksana Zelenuk <oksana@zelenuk.com>
2016-10-12 11:35:43 +0300

    ukrdez_20161012_

--- ukrdez/js/script.js
+++ ukrdez/js/script.js
@@ -132,3 +132,14 @@ jQuery(function ($) {
   		columnWidth: 50,
 	});
 });
+
+$(function() {
+   $('.jcarousel-pagination').on('jcarouselpagination:active', 'a', function() {
+            $(this).addClass('active');
+          })
+            .on('jcarouselpagination:inactive', 'a', function() {
+              $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+
+});
