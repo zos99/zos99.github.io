b171137475154a7c6675998da5f5a6d64c7cd7d8
Oksana Zelenjuk <oksana@zelenuk.com>
2017-03-27 21:11:06 +0300

    donenergo

--- donenergo/js/main.js
+++ donenergo/js/main.js
@@ -1,16 +1,1 @@
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
-$(document).ready(function(){
-    $("#menu1").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
+
