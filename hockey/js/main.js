c75c7be0e3b29a7f8e30077fa0d22ce4980cd890
Oksana Zelenuk <oksana@zelenuk.com>
2017-05-16 17:02:23 +0300

    hockey_20170516

--- hockey/js/main.js
+++ hockey/js/main.js
@@ -6,23 +6,7 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
-$(document).ready(function(){
-    $("#menu1").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            console.log(data.curTop);
-        }
-    });
-} )( jQuery );
+
 $(document).ready(function(){
 
 	$(".main").onepage_scroll({
