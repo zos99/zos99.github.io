203f10e21129ceaf2d98a61e44635111c7ac7e80
Oksana Zelenjuk <oksana@zelenuk.com>
2017-05-26 01:02:49 +0300

    hockey_20170525_8

--- hockey/js/skrollefy.js
+++ hockey/js/skrollefy.js
@@ -4,26 +4,3 @@ $(function() {
 		$.scrollify("move",$(this).attr("href"));
 	});
 });
-
-/*-------*/
-
-$.fn.touchScrolling = function(){
-    var startPos = 0,
-		self = $(this);
-
-    self.bind('touchstart', function(event) {
-        var e = event.originalEvent;
-        startPos = self.scrollTop() + e.touches[0].pageY;
-        e.preventDefault();
-    });
-
-    self.bind('touchmove', function(event) {
-        var e = event.originalEvent;
-        self.scrollTop(startPos - e.touches[0].pageY);
-        e.preventDefault();
-    });
-};
-
-$(function() {
-    $('#touch-scroll').touchScrolling();
-})
