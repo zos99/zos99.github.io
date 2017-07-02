39d8eeaf6761b3ae8b8fa78b461125109f647168
Oksana Zelenjuk <oksana@zelenuk.com>
2017-07-03 01:43:17 +0300

    20170703

--- beregletom/js/script.js
+++ beregletom/js/script.js
@@ -69,3 +69,38 @@ $(document).ready(function(){
         $('.box-height ',this).height(highestBox);
     });
 });
+//--------------main-menu
+$(document).ready(function(){
+
+    var $menu = $("#menu");
+
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+            $menu.fadeOut('fast',function(){
+                $(this).removeClass("default")
+                       .addClass("fixed transbg")
+                       .fadeIn('fast');
+                       $(".logo-miny").css("display","block");
+            });
+        } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+            $menu.fadeOut('fast',function(){
+                $(this).removeClass("fixed transbg")
+                       .addClass("default")
+                       .fadeIn('fast');
+                        $(".logo-miny").css("display","none");
+            });
+        }
+    });//scroll
+
+    $menu.hover(
+        function(){
+            if( $(this).hasClass('fixed') ){
+                $(this).removeClass('transbg');
+            }
+        },
+        function(){
+            if( $(this).hasClass('fixed') ){
+                $(this).addClass('transbg');
+            }
+        });//hover
+});//jQuery
