3867f509d48f5487a3958bb4ea9749f94b753e05
Oksana Zelenjuk <oksana@zelenuk.com>
2017-08-31 14:34:30 +0300

    new_20170831

--- kata-log_1/js/main.js
+++ kata-log_1/js/main.js
@@ -14,3 +14,30 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+
+//-----------services-box-----------------
+ /* Function for hover on services-box*/
+$(function() {
+  function servicesBoxHover() {
+  $( '.services-box__image-container').mouseover(function() {
+    $(this).css('cursor', 'pointer');
+    $(this).find('.services-box__image').css('opacity', '0.1').css('transition', '.50s');
+    $(this).find('.services-box__v-line').css('display', 'block');
+
+  });
+  $( '.services-box__image-container').mouseout(function() {
+      $(this).find('.services-box__image').css('opacity', '1');
+      $(this).find('.services-box__v-line').css('display', 'none');
+    });
+  }
+  servicesBoxHover();
+});
+//*Скрипт, привязывающий событие click, открывающее модальное окно, к элементам, имеющим класс .btn
+$(document).ready(function(){
+  //при нажатию на любую кнопку, имеющую класс .btn
+  $(".online-booking").click(function() {
+    //открыть модальное окно с id="myModal"
+    $("#myModal").modal('show');
+  });
+});
