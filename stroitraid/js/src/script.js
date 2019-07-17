779baf10beb6409ac751c554492adb4f3b0f0127
Oksana Zelenuk <oksana@zelenuk.com>
2019-07-17 22:13:37 +0300

    201907174

--- stroitraid/js/src/script.js
+++ stroitraid/js/src/script.js
@@ -1,3 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+     pagination: '.swiper-pagination',
+     paginationClickable: true,
+     nextButton: '.swiper-button-next',
+     prevButton: '.swiper-button-prev',
+     parallax: true,
+     speed: 1000,
+     spaceBetween: 30,
+     effect: 'fade',
+     autoplay: 2500,
+     autoplayDisableOnInteraction: false,
+     loop: true
+ });
   var $ = jQuery.noConflict();
   $(document).ready(function() {
   $().UItoTop({ easingType: 'easeOutQuart' });
